2d2a370e1361baf1358e09400f7647628551a1a6
Corey Brown <corey@coreyb.net>
2026-09-17 23:34:05 -0400

    build: update Loom for Gradle 9

--- buildSrc/src/main/kotlin/dev/engine_room/gradle/transitive/TransitiveSourceSetsExtension.kt
+++ buildSrc/src/main/kotlin/dev/engine_room/gradle/transitive/TransitiveSourceSetsExtension.kt
@@ -33,9 +33,9 @@ open class TransitiveSourceSetsExtension(val project: Project) {
 
         transitives.forEach { (sourceSet, configurator) ->
             project.configurations.named(sourceSet.compileOnlyConfigurationName).configure {
-                extendsFrom(configs[sourceSet])
+                extendsFrom(requireNotNull(configs[sourceSet]))
                 configurator.compileSourceSets.forEach {
-                    extendsFrom(configs[it])
+                    extendsFrom(requireNotNull(configs[it]))
                 }
             }
         }
@@ -51,9 +51,9 @@ open class TransitiveSourceSetsExtension(val project: Project) {
 
         transitives.forEach { (sourceSet, configurator) ->
             project.configurations.named(sourceSet.runtimeOnlyConfigurationName).configure {
-                extendsFrom(configs[sourceSet])
+                extendsFrom(requireNotNull(configs[sourceSet]))
                 configurator.runtimeSourceSets.forEach {
-                    extendsFrom(configs[it])
+                    extendsFrom(requireNotNull(configs[it]))
                 }
             }
         }
